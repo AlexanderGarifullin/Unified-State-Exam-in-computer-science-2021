c2b845cf99bab5ec53c687e53543c9f3c4504dfc
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2023-07-18 20:54:30 +0500

    Solving problems 5 from the site kpolyakov.spb.ru using Pascal

---
+++ 5/5.1.pas
@@ -0,0 +1,15 @@
+﻿var x,i : integer;
+  a:array[0..1000] of integer;
+begin
+  for i := 0 to 1000 do a[i] := 0;
+  for i := 20 to 600 do
+    begin
+      x := i;
+      x := x div 4;
+      a[x] += 1;
+    end;
+  x := 0;
+  for i := 0 to 1000 do
+    if a[i] <> 0 then x += 1;
+  writeln(x);
+end.
