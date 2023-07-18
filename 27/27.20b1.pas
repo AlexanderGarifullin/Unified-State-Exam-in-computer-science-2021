80837446e5c01190dc5232ed81b8880dc27a8528
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2023-07-18 21:09:31 +0500

    Solving problems 27 from the site kpolyakov.spb.ru using Pascal

---
+++ 27/27.20b1.pas
@@ -0,0 +1,31 @@
+﻿var a1, a,  b1, b,  mx, c,t,i,n:integer;
+begin
+  //assign(input,'27-20b.txt');
+  readln(n);
+  a1 := 0;  b1 := 0;  mx := 0;  c:= 1;
+  for i := 1 to n do
+    begin
+      readln(a,b);
+      if (b1 = a)  then
+        begin
+          c += 1;
+          mx := max(mx,c);
+          t := a1;  a1 := a;  a := t;
+          t := b1;  b1 := b;  b := t;
+        end
+      else if b1 = b then
+        begin
+          c += 1;
+          mx := max(mx,c);
+          t := a1;  a1 := b;  b := t;
+          t := b1;  b1 := a;  a := t;
+        end
+      else 
+        begin
+          c := 1;
+          t := a1;  a1 := b;  b := t;
+          t := b1;  b1 := a;  a := t;
+        end;
+    end;
+  writeln(mx);  
+end.
---
+++ 27/27.20b2.pas
@@ -0,0 +1,47 @@
+﻿var n,  i, i2, a,  b,  a0, b0, a20,  b20,  c,  m:integer;
+begin
+  assign(input,'27-20b.txt');
+  readln(n);
+  readln(a0,b0);  c := 1; m := 0;
+  a20 := a0;  b20 := b0;
+  for i := 2 to n do
+    begin
+      readln(a,b);
+      if (i >= 50176) and (i <= 50180) then writeln('a=',a,' ''b=',b,' ','a0=',a0,' ''b0=',b0,' ''a20=',a20,' ''b20=',b20);
+      if b0 = a then
+        begin
+          c += 1;
+          if c > m then begin m := max(m,c); i2 := i;end;
+          a0 := a;  b0 := b;
+        end
+      else if b0 = b then
+        begin
+          c += 1;
+          if c > m then begin m := max(m,c); i2 := i;end;
+          a0 := b; b0 := a;
+        end
+        
+      else if b20 = a then
+        begin
+          c += 1;
+         if c > m then begin m := max(m,c); i2 := i;end;
+          a20 := a;  b20 := b;
+        end
+      else if b20 = b then
+        begin
+          c += 1;
+          if c > m then begin m := max(m,c); i2 := i;end;
+          a20 := b; b20 := a;
+        end  
+      else
+        begin
+          if c > m then begin m := max(m,c); i2 := i;end;
+          c := 1;
+          a0 := a;  b0 := b;
+          a20 := b; b20 := a;
+        end;
+       if (i >= 50176) and (i <= 50180) then begin writeln('a=',a,' ''b=',b,' ','a0=',a0,' ''b0=',b0,' ''a20=',a20,' ''b20=',b20); writeln; end;
+       //writeln;
+    end;
+  writeln(m,' ',i2);    
+end.
